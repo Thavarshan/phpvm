ab0e5be77667fce15de195dcf5a393140509dc0b
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2024-10-05 22:00:34 +0100

    Refactor installPHP test and remove skip

--- tests/install.test.js
+++ tests/install.test.js
@@ -31,7 +31,7 @@ jest.mock('path', () => ({
   resolve: jest.fn(),
 }));
 
-describe.skip('installPHP', () => {
+describe('installPHP', () => {
   const version = '7.4.10';
   const tarballPath = '/path/to/php-7.4.10.tar.gz';
   const platform = 'macos-apple-silicon';
